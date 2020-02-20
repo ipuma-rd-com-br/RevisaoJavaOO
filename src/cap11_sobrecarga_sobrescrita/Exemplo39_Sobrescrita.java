4790afc13020efcbb057710d128b361bc61bdf88
Ivo <ipuma@rd.com.br>
2020-02-20 20:43:52 -0300

    finalizando exemplo 39

--- src/cap11_sobrecarga_sobrescrita/Exemplo39_Sobrescrita.java
+++ src/cap11_sobrecarga_sobrescrita/Exemplo39_Sobrescrita.java
@@ -11,5 +11,14 @@ public class Exemplo39_Sobrescrita {
         UmaSubClasse subclasse = new UmaSubClasse();
         subclasse.umMetodo(); // método herdado de UmaSuperClasse
         subclasse.outroMetodo(); // método sobrescrito em UmaSubClasse
+
+        /*
+        OBSERVAÇÕES:
+
+        UmaSubClasse é uma classe que é herdeira de UmaSuperClasse, logo herda todos seus métodos e atributos. No entanto,
+        UmaSubClasse sobrescreve o método outroMetodo(), realizando uma implementação diferente da definida na super-classe.
+
+        Verifiquem o código de UmaSubClasse e UmaSuperClasse.
+         */
     }
 }
